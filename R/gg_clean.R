9a2d3626526526f01a1b941535876eee71e1ccf8
Michael Clark <micl@umich.edu>
2018-06-12 08:24:54 -0400

    more cleanup and prep for package site

--- R/gg_clean.R
+++ R/gg_clean.R
@@ -1,31 +1,51 @@
-#' Clean visualizations
+#' Clean visualization themes
 #'
 #' @description Clean up plots from their defaults.
 #'
-#' @param vis A plotly, or ggvis
-#' @param MB For plotly, an option to dispaly the mode bar. Defaults to FALSE.
+#' @param vis A visualization created by \link[ggplot2]{ggplot2} or
+#'   \link[plotly]{plotly}.
+#' @param MB For plotly, an option to display the mode bar. Defaults to FALSE.
+#'
 #' @details From a gray background, to unnecessary gridlines, to by-default
 #'   reference lines, some of the more popular visualization packages come out
-#'   75% fantastic and 25% wtf?.  These functions remove unnecessary gridlines,
-#'   'de-bold' the blacks, etc. **ggplot2**: This function takes a ggplot object
+#'   75% fantastic and 25% questionable/arbitrary/problematic.  These functions
+#'   remove unnecessary gridlines, 'de-bold' the blacks, etc.
+#'
+#'   - **ggplot2**: \code{theme_trueMinimal} This function takes a ggplot object
 #'   and removes the gray background, gridlines and adds opacity to the default
 #'   black axes and labels, allowing the pattern of the visual to be expressed
 #'   in unimpeded fashion.
 #'
-#'   Plotly will put reference lines at zero, and some of its 'modebar' is
-#'   unnecessary.  Otherwise little is changed at this point, except for
-#'   theme_blank, which is like theme_void for ggplot.
+#'   - **plotly**: \code{theme_plotly}, \code{theme_blank} removes reference
+#'   lines at zero, and some of its 'modebar' is unnecessary.  Otherwise little
+#'   is changed at this point, except for theme_blank, which is like theme_void
+#'   for ggplot.
+#' @note
+#' You may continue to override any aspect of these themes. For example with
+#' ggplot2, you would just add a theme afterward just like you would any other
+#' plot.
+#'
 #' @importFrom magrittr '%>%'
 #'
 #' @examples
 #' library(visibly)
 #' library(ggplot2)
+#'
 #' data(mtcars)
-#' # currently ggplot is having a problem with rlang so this is commented until they fix it
-#' # ggplot(aes(wt, mpg), data=mtcars) + theme_trueMinimal()
+#'
+#' ggplot(aes(wt, mpg), data=mtcars) +
+#'   geom_point() +
+#'   labs(title='Plot') +
+#'   theme_trueMinimal()
+#'
 #' library(plotly)
-#' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_plotly()
-#' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_blank()
+#' mtcars %>%
+#'   plot_ly(x=~wt, y=~mpg, mode='markers') %>%
+#'   theme_plotly()
+#'
+#' mtcars %>%
+#'   plot_ly(x=~wt, y=~mpg, mode='markers') %>%
+#'   theme_blank()
 
 
 
@@ -34,6 +54,7 @@ theme_trueMinimal <- function(){
   ggplot2::theme(
     axis.text.x = ggplot2::element_text(colour='gray50'),
     axis.text.y = ggplot2::element_text(colour='gray50'),
+    title = ggplot2::element_text(colour='gray33'),
     legend.key = ggplot2::element_rect(fill='transparent', colour = NA),
     legend.background = ggplot2::element_rect(fill='transparent', colour = NA),
     panel.background = ggplot2::element_blank(),
--- README.md
+++ README.md
@@ -3,66 +3,21 @@ Status](https://travis-ci.org/m-clark/visibly.svg?branch=master)](https://travis
 [![Coverage
 Status](https://img.shields.io/codecov/c/github/m-clark/visibly/master.svg)](https://codecov.io/github/m-clark/visibly?branch=master)
 
-<!-- README.md is generated from README.Rmd. Please edit that file -->
-
 # visibly
 
 <img src="man/figures/visibly_hex.png" align="right" width = 200/>
 
-Visibly is a handful of functions I use for color palettes and themes in
-R.
+Visibly is a handful of functions I use for color palettes, themes, etc.
+in R. Inside you will find:
 
-## Example
+  - some ready-made palettes, e.g. based on R blue and Stan red
+  - a function to quickly and easily create palettes with using
+    `colortools::complementary` `colortools::adjacent` etc.
+  - clean, web-friendly themes for ggplot2 and plotly
+  - a function to interact with
+    [colorgorical](http://vrl.cs.brown.edu/color/)
 
-Create a palette.
-
-``` r
-library(visibly)
-create_palette('papayawhip')
-$papayawhip
-[1] "papayawhip"
-
-$complementary
-[1] "#FFEFD5" "#D5E5FF"
-
-$analogous
-[1] "#FFEFD5" "#FAFFD5" "#FFDAD5"
-
-$split_complentary
-[1] "#FFEFD5" "#D5FAFF" "#DAD5FF"
-
-$triadic
-[1] "#FFEFD5" "#D5FFEF" "#EFD5FF"
-
-$square
-[1] "#FFEFD5" "#D5FFDA" "#D5E5FF" "#FFD5FA"
-
-$tetradic
-[1] "#FFEFD5" "#E5FFD5" "#D5E5FF" "#EFD5FF"
-```
-
-A paletted based on the blue in the R logo.
-
-``` r
-palettes$Rblue
-$Rblue
-[1] "#1f65b7"
-
-$complementary
-[1] "#1f65b7" "#b7701f"
-
-$monochromatic
-[1] "#1f65b7" "#366caa" "#4a719e" "#5a7491"
-
-$analagous
-[1] "#1f65b7" "#241fb7" "#1fb2b7"
-
-$split_complementary
-[1] "#1f65b7" "#b2b71f" "#b7241f"
-
-$triadic
-[1] "#1f65b7" "#66b71f" "#b71f66"
-
-$tetradic
-[1] "#1f65b7" "#b7701f" "#66b71f" "#701fb7"
-```
+Visibly is currently in its very early stages, so more may be added
+soon. For some additional palettes for those fond of another time, you
+might be interested in
+[NineteenEightyR](https://github.com/m-clark/NineteenEightyR).
